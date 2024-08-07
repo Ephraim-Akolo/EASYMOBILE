5f72e25496591ea4747edaa41b71950c0b5b8bf4
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-08-07 13:09:06 +0100

    added documentation

--- easyb2b/utils.py
+++ easyb2b/utils.py
@@ -1,10 +1,20 @@
 from datetime import datetime
 import random
 
+# Define the alphanumeric characters
 _alphnum = [chr(i) for i in range(48, 58)] + [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
 
-def get_easyb2b_reference(code_len=16) -> str:
-    date = datetime.now().strftime("%Y%m%d%I%M")
-    code = ''.join(random.sample(_alphnum, code_len))
-    return f"{date}{code}"
+def get_easyb2b_reference(code_len: int = 16) -> str:
+    """
+    Generate a unique reference code for a transaction by combining the current date-time string and a random alphanumeric code.
+    Args:
+        code_len (int, optional): The length of the random alphanumeric code. Default is 16.
+        
+    Returns:
+        str: The generated reference code.
+    """
+    date_str = datetime.now().strftime("%Y%m%d%I%M")
+    random_code = ''.join(random.sample(_alphnum, code_len))    
+    return f"{date_str}{random_code}"
+
 
