1eb11cd868edacfe98f4c9586e415969bf1bb489
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-08-11 10:48:15 +0100

    linked to services

--- easymobile/client.py
+++ easymobile/client.py
@@ -1,4 +1,4 @@
-from .base import EasyBase
+from . import EasyBase, EasyAirtime, EasyData, EasyCable
 
 
 class EasyMobile(EasyBase):
@@ -16,4 +16,30 @@ class EasyMobile(EasyBase):
             timeout (float, optional): Timeout for the requests. Default is 45.1 seconds.
         """
         super().__init__(api_key, timeout)
+        self._api_key = api_key
+
+
+    @property
+    def easy_airtime(self):
+        try:
+            return self._easy_airtime_
+        except AttributeError:
+            self._easy_airtime_ = EasyAirtime(self._api_key, self.timeout)
+        return self._easy_airtime_
     
+    @property
+    def easy_data(self):
+        try:
+            return self._easy_data_
+        except AttributeError:
+            self._easy_data_ = EasyData(self._api_key, self.timeout)
+        return self._easy_data_
+    
+    @property
+    def easy_cable(self):
+        try:
+            return self._easy_cable_
+        except AttributeError:
+            self._easy_cable_ = EasyCable(self._api_key, self.timeout)
+        return self._easy_cable_
+        
